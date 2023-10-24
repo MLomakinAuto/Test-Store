0d6d00734ce6ff33dc87bdf531d26be222b3a7f9
MykolaL <kolkalomakin@gmail.com>
2023-10-24 10:13:11 +0300

    selenium test

---
+++ unit_tests/src/test/java/selenium/Locators.java
@@ -0,0 +1,11 @@
+package selenium;
+
+import org.openqa.selenium.By;
+
+public class Locators {
+    public static final By EMAIL_INPUT = By.id("identifierId");
+    public static final By NEXT_BUTTON = By.id("identifierNext");
+    public static final By PASSWORD_INPUT = By.xpath("//input[@type='password']");
+    public static final By PASSWORD_NEXT_BUTTON = By.id("passwordNext");
+    public static final By VALIDATION_ERROR = By.xpath("//span[contains(text(),'Wrong password. Try again or click Forgot password to reset it.')]");
+}
