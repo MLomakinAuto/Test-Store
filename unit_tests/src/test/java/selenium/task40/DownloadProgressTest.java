a266ff55dd9f2cb066eff04223daa6c254fdc4ba
MykolaL <kolkalomakin@gmail.com>
2023-11-23 19:47:42 +0200

    fix v2

--- unit_tests/src/test/java/selenium/task40/DownloadProgressTest.java
+++ unit_tests/src/test/java/selenium/task40/DownloadProgressTest.java
@@ -1,5 +1,6 @@
 package selenium.task40;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -11,15 +12,17 @@ public class DownloadProgressTest extends BaseTest {
 
     @Test
     public void testDownloadProgress() {
+
         driver.get("https://demo.seleniumeasy.com/bootstrap-download-progress-demo.html");
 
         WebElement downloadButton = driver.findElement(By.id("cricle-btn"));
         downloadButton.click();
 
-        WebDriverWait wait = new WebDriverWait(driver, 60);
-
-        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//div[@class='percenttext']"), "50%"));
-
+        WebDriverWait wait = new WebDriverWait(driver, 60, 2);
+        WebElement progressBar = driver.findElement(By.xpath("//div[@class='percenttext']"));
+        wait.until(ExpectedConditions.textToBePresentInElement(progressBar, "50%"));
+        String progressText = progressBar.getText();
+        Assertions.assertEquals("50%", progressText, "Download progress did not reach 50% as expected.");
         driver.navigate().refresh();
     }
 }
