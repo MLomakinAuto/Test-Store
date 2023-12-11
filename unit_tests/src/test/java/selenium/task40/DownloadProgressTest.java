2367ecd214a912c292b971578bb26849700db683
MykolaL <kolkalomakin@gmail.com>
2023-12-11 11:39:44 +0200

    Task60Part1

--- unit_tests/src/test/java/selenium/task40/DownloadProgressTest.java
+++ unit_tests/src/test/java/selenium/task40/DownloadProgressTest.java
@@ -2,27 +2,28 @@ package selenium.task40;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import selenium.helpers.BaseTest;
 
 public class DownloadProgressTest extends BaseTest {
 
     @Test
     public void testDownloadProgress() {
+        WebDriver driver = new ChromeDriver();
 
-        driver.get("https://demo.seleniumeasy.com/bootstrap-download-progress-demo.html");
+        try {
+            driver.get("https://demo.seleniumeasy.com/bootstrap-download-progress-demo.html");
 
-        WebElement downloadButton = driver.findElement(By.id("cricle-btn"));
-        downloadButton.click();
+            DownloadProgressPage downloadProgressPage = new DownloadProgressPage(driver);
+            downloadProgressPage.clickDownloadButton();
 
-        WebDriverWait wait = new WebDriverWait(driver, 60, 2);
-        WebElement progressBar = driver.findElement(By.xpath("//div[@class='percenttext']"));
-        wait.until(ExpectedConditions.textToBePresentInElement(progressBar, "50%"));
-        String progressText = progressBar.getText();
-        Assertions.assertEquals("50%", progressText, "Download progress did not reach 50% as expected.");
-        driver.navigate().refresh();
+            downloadProgressPage.waitForDownloadProgressToReach("50%");
+            String progressText = downloadProgressPage.getProgressText();
+
+            Assertions.assertEquals("50%", progressText, "Download progress did not reach 50% as expected.");
+        } finally {
+            driver.quit();
+        }
     }
 }
