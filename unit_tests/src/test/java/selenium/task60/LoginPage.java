2a84aa490e857806686f924cad375026175774f2
MykolaL <kolkalomakin@gmail.com>
2023-12-18 09:54:02 +0200

    fix

--- unit_tests/src/test/java/selenium/task60/LoginPage.java
+++ unit_tests/src/test/java/selenium/task60/LoginPage.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
     private WebDriver driver;
@@ -20,9 +22,16 @@ public class LoginPage {
     @FindBy(id = "send2")
     private WebElement signUp;
 
+    @FindBy(css = ".logged-in")
+    private WebElement welcomeMessage;
+
     public LoginPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
+    }
+
+    public String getWelcomeMessage(WebDriverWait wait) {
+        return wait.until(ExpectedConditions.visibilityOf(welcomeMessage)).getText();
     }
 
     public void login(String username, String password) {
--- unit_tests/src/test/java/selenium/task60/LogoutTest.java
+++ unit_tests/src/test/java/selenium/task60/LogoutTest.java
@@ -2,21 +2,21 @@ package selenium.task60;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import selenium.helpers.BaseTest;
 
-public class LoginTest extends BaseTest {
+public class LogoutTest extends BaseTest {
 
     @Test
-    public void testLoginLogout() {
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+    public void logoutTest(){
         driver.get("https://magento.softwaretestingboard.com/");
         LoginPage loginPage = new LoginPage(driver);
+        LogoutPage logoutPage = new LogoutPage(driver);
         loginPage.login("mykolalomakin854@gmail.com", "Qwerty12345");
-
         String expectedWelcomeMessage = "Welcome, qqqq qqqq!";
-        String actualWelcomeMessage = loginPage.getWelcomeMessage(wait);
-        Assertions.assertEquals(expectedWelcomeMessage, actualWelcomeMessage, "Login was not successful");
+        logoutPage.logout();
+        String pageSource = driver.getPageSource();
+        boolean isWelcomeMessagePresent = pageSource.contains(expectedWelcomeMessage);
+        Assertions.assertFalse(isWelcomeMessagePresent, "Welcome message is not present on the page.");
     }
 
 }
